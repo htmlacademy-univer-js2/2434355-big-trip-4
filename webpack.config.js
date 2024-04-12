3ba7a541ade9c68ce5267138ba4c50f9229c4a32
Dariaeg <dariaeg04@gmail.com>
2024-04-12 17:46:14 +0500

    babel + хэширование

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,40 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const { Template } = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   devtool: 'source-map',
   plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [{ from: 'public',
+      globOptions: {
+        ignore:['**/index.html'],
+      }
+
+    }],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
 };
