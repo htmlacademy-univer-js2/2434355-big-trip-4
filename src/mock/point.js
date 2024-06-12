e8ecc4cffeb4b89f49c61fc992c324fcad9c67e3
Dariaeg <dariaeg04@gmail.com>
2024-06-13 03:14:55 +0500

    2

--- src/mock/point.js
+++ src/mock/point.js
@@ -2,7 +2,7 @@ import { getRandomArrayElement } from '../utils/common.js';
 import { POINT_TYPES } from '../const.js';
 
 const points = [{
-  basePrice: 1010,
+  basePrice: 1000,
   dateFrom: '2019-07-10T12:55:56.845Z',
   dateTo: '2019-07-10T13:35:56.845Z',
   destination: 0,
@@ -11,7 +11,7 @@ const points = [{
     'b4c3e4e6-9053-42ce-b747-e281314baa33'],
   type: POINT_TYPES[1]
 }, {
-  basePrice: 1100,
+  basePrice: 1250,
   dateFrom: '2020-08-10T10:25:56.845Z',
   dateTo: '2020-08-10T15:55:56.845Z',
   destination: 1,
@@ -19,7 +19,7 @@ const points = [{
   offers: [],
   type: POINT_TYPES[0]
 }, {
-  basePrice: 1200,
+  basePrice: 1300,
   dateFrom: '2021-08-05T10:25:56.845Z',
   dateTo: '2021-08-08T15:55:56.845Z',
   destination: 2,
@@ -27,7 +27,7 @@ const points = [{
   offers: [],
   type: POINT_TYPES[2]
 }, {
-  basePrice: 1300,
+  basePrice: 1500,
   dateFrom: '2020-08-05T10:25:56.845Z',
   dateTo: '2021-08-08T15:55:56.845Z',
   destination: 3,
