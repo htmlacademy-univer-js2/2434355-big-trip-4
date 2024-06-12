c5e5c02872fe57903334ed8d6574296bea998fc7
Dariaeg <131648019+Dariaeg@users.noreply.github.com>
2024-06-13 03:23:28 +0500

    Update destinations-api-service.js

--- src/api-services.js/destinations-api-service.js
+++ src/api-services.js/destinations-api-service.js
@@ -1,7 +1,7 @@
-import ApiService from "../framework/api-service";
+import ApiService from '../framework/api-service';
 
 export default class DestinationsApiService extends ApiService {
   get destinations() {
-    return this._load({ url: "destinations" }).then(ApiService.parseResponse);
+    return this._load({ url: 'destinations' }).then(ApiService.parseResponse);
   }
 }
